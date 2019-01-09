527e0a4be3b83e9c31b41a335a0e904d694ac44c
Tyler Garlick <tgarlick@heliotraining.com>
2019-01-09 10:55:01 -0700

    Work on GraphQL Setup
    - Setup the initial schemas

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { GraphQLServer } from 'graphql-yoga'
+import * as GraphQLJSON from 'graphql-type-json'
+
+const typeDefs = [`schema.graphql`]
+
+const resolvers = {
+  JSON: GraphQLJSON,
+  Query: {
+    hello: (_, { name }) => {
+      const returnValue = `Hello ${name || 'World!'}`
+      return returnValue
+    }
+  },
+
+  Mutation: {
+    async transform(root, args, context, info) {
+      return await {
+        output: []
+      }
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
